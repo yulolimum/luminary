c7a8338427dcd4cfc6b52e4b3e2199f0feeb52b3
Yulian Glukhenko <yulian@infinite.red>
2025-02-17 05:45:24 -0800

    Adds prettier.

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-packagejson'],
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}

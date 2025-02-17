477d28777d229e3fe1efe36fe1a24f2ccb8a6b73
Yulian Glukhenko <yulian@infinite.red>
2025-02-17 05:55:58 -0800

    Adds eslint and configures basic rules.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # luminary
 
-
-
-
 `./scripts/setup`
---
+++ eslint.config.mjs
@@ -0,0 +1,90 @@
+import eslint from '@eslint/js'
+import prettier from 'eslint-plugin-prettier/recommended'
+import react from 'eslint-plugin-react'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  // global settings
+  {
+    ignores: ['.yarn/**', 'node_modules/**', 'packages/*/node_modules/**'],
+  },
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node, node: true, NodeJS: true } },
+    settings: { react: { version: 'detect' } },
+  },
+
+  // javascript
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    rules: {
+      ...eslint.configs.recommended.rules,
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  // typescript
+  ...tseslint.configs.recommended.map(c => ({ ...c, files: ['**/*.{ts,tsx}'] })),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(),
+        allowDefaultProject: ['packages/app/**', 'packages/core/**', 'packages/theme/**'],
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  // react
+  {
+    files: ['**/*.{tsx,jsx}'],
+    plugins: { react: react.configs.flat.recommended.plugins.react },
+    rules: {
+      ...react.configs.flat.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/display-name': 'off',
+    },
+  },
+
+  // global rules
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  prettier,
+)
